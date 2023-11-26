dddb9fde36ad5664a3fadbf24135266b674ac813
Aleksey <yashnikov88@mail.ru>
2023-11-26 22:49:15 +0300

    Написали решение 3 задачи

---
+++ Workshop3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+ 
+int size = new Random().Next(1, 11);
+
+System.Console.WriteLine($"Количетсов элементов массива: {size}"); // Указывается сколько элементов в массиве
+
+double[] FillSourceArray(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().NextDouble() * 100;
+    }
+    return array;
+}
+
+void PrintDiffMinMax(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine(arr[i]);
+    }
+    double difference = arr.Max() - arr.Min(); // Считается разность "difference" между максимальным и минимальным элементом массива
+    
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Минимальное значение массива {arr.Min()}, максимальное значение массива {arr.Max()}");
+    System.Console.WriteLine($"Разница между минимальным и максимальным значениями массива составляет {difference}");
+    System.Console.WriteLine();
+}
+
+PrintDiffMinMax(FillSourceArray(size));
+System.Console.WriteLine();
