f56fa8fc14d7da93459620843c72bcc14e478b0c
Aleksey <yashnikov88@mail.ru>
2023-11-28 16:06:22 +0300

    Написали решение 3 задачи

--- Workshop3/obj/Debug/net8.0/Workshop3.AssemblyInfo.cs
+++ Workshop3/obj/Debug/net8.0/Workshop3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Workshop3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174d907d5b5e378bd33ba74aff82ca10427f79c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dddb9fde36ad5664a3fadbf24135266b674ac813")]
 [assembly: System.Reflection.AssemblyProductAttribute("Workshop3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Workshop3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
